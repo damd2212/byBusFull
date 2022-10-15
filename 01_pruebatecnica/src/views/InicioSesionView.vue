d219de1826bfe86e8772d6cdbc137f2f142f6fa2
damd2212 <53495267+damd2212@users.noreply.github.com>
2022-10-15 16:16:13 -0500

    rimer commit

---
+++ 01_pruebatecnica/src/views/InicioSesionView.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Inicio sesion</h1>
+</template>
+<script>
+export default {
+    name: "FuncionFibonacciView",
+    mounted() {
+        document.title = "Inicio Sesion";
+    }
+};
+</script>
+<style scoped>
+
+</style>
+
